fdc8498f45cc8d792d74dc6b96a6e4e1ad7c82b2
DeveloperHubNet <develperhub@gmail.com>
2023-04-24 09:49:37 +0900

    Files added

---
+++ QSSAPI/BLL/BLL_Location.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using QSSAPI.DAL;
+using QSSAPI.BOL;
+
+
+namespace QSSAPI.BLL
+{
+    public class BLL_Location
+    {
+        public DataTable BindLocation()
+        {
+            DAL_Location dalLocation = new DAL_Location();
+            return dalLocation.BindLocation();
+        }
+
+        public DataTable BindLocationByCode(string code)
+        {
+            DAL_Location dalLocation = new DAL_Location();
+            return dalLocation.BindLocationByNumber(code);
+        }
+
+        public DataTable BindLocationByName(string name)
+        {
+            DAL_Location dalLocation = new DAL_Location();
+            return dalLocation.BindLocationByName(name);
+        }
+
+        public int InserLocation(BOL_Location bolLocation)
+        {
+            int effectID = 0;
+            try
+            {
+                SqlConjunction.StartTransaction();
+
+                DAL_Location dalLocation = new DAL_Location();
+                effectID = dalLocation.Insert_Location(bolLocation);
+
+                SqlConjunction.CommitTransaction();
+            }
+            catch (Exception ex)
+            {
+                SqlConjunction.RollbackTransaction(ex.Message);
+
+            }
+            return effectID;
+        }
+    }
+}
---
+++ QSSAPI/BOL/BOL_Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QSSAPI.BOL
+{
+    public class BOL_Location
+    {
+        public int loc_ID { get; set; }
+        public string loc_Code { get; set; }
+        public string loc_Name { get; set; }
+        public int loc_BranchID { get; set; }
+        public  string loc_createby { get; set; }
+        public DateTime loc_createdate { get; set; }
+        public string loc_updateby { get; set; }
+        public DateTime loc_LastUpdate { get; set; }
+        public int loc_inactive { get; set; }
+    }
+}
